142ca75080db3bb4c06610df42b1814720bb5fd5
huyhuy <huynguyen29122000@gmail.com>
2021-06-02 03:30:25 +0700

    Assignment-02

---
+++ js.js
@@ -0,0 +1,238 @@
+
+class Subject{
+    constructor(title,mark,title2,mark2){
+        this.subject = [
+            {
+                title: title,
+                mark: mark
+            },
+            {
+                title: title2,
+                mark: mark2
+            }
+        ]
+    }
+    show(){
+        console.log(this.subject)
+    }
+}
+
+class Student{
+    constructor(name,gender,mark,subject){
+        this.name = name
+        this.gender = gender
+        this.mark = mark
+        this.subject = subject
+    }
+    showInfo(){
+        console.log(`${this.name}`-`${this.mark}`)
+    }
+}
+    
+    var st2 = new Student('Huy1','FeMale',9,new Subject('Toan',1,'Ly',9))
+    var st9 = new Student('Huy8','Male',2,new Subject('Toan',8,'Ly',2))
+    var st4 = new Student('Huy3','FeMale',7,new Subject('Toan',3,'Ly',7))
+    var st7 = new Student('Huy6','Male',4,new Subject('Toan',6,'Ly',4))
+    var st3 = new Student('Huy2','FeMale',8,new Subject('Toan',2,'Ly',8))
+    var st5 = new Student('Huy4','Male',6,new Subject('Toan',4,'Ly',6))
+    var st0 = new Student('Huy10','Male',0,new Subject('Toan',10,'Ly',0))
+    var st6 = new Student('Huy5','FeMale',5,new Subject('Toan',5,'Ly',5))
+    var st8 = new Student('Huy7','Male',3,new Subject('Toan',7,'Ly',3))
+    var st1 = new Student('Huy','Male',10,new Subject('Toan',0,'Ly',10))
+    var st10 = new Student('Huy9','Male',1,new Subject('Toan',9,'Ly',1))
+    
+    class MangerStudent{
+        constructor(){
+            this.list =[]
+        }
+        addStudent(e){
+            this.list.push(e)
+        }
+        print(){
+            console.log(this.list)
+        }
+        sortByASC(){
+            var temp
+            for(var i =0;i<this.list.length;i++){
+                for(var j=0;j<this.list.length;j++){
+                    if(this.list[i].mark>this.list[j].mark){
+                        temp = this.list[i]
+                        this.list[i]=this.list[j]
+                        this.list[j]=temp
+                    }
+                }
+            }
+        }
+
+        sortByDESC(){
+            var temp
+            for(var i =0;i<this.list.length;i++){
+                for(var j=0;j<this.list.length;j++){
+                    if(this.list[i].mark<this.list[j].mark){
+                        temp = this.list[i]
+                        this.list[i]=this.list[j]
+                        this.list[j]=temp
+                    }
+                }
+            }
+        }
+
+    }
+
+    const mn = new MangerStudent()
+    
+
+    const $ = document.querySelector.bind(document)
+    const $$ = document.querySelectorAll.bind(document)
+    const $addBtn = $('.btn-add')
+    const $SearchBtn = $('.btn-sch')
+    const $ASC = $('.ASC')
+    const $DESC = $('.DESC')
+    const $reset = $('.reset-btn')
+    var sub1 = 'Toan'
+    var sub2 = 'Ly'
+    var arr =[]
+    var arr_sch =[]
+   
+    const app ={
+        
+        reset: function(){
+            const  $this = this
+            $reset.onclick = ()=>{
+                arr_sch =[]
+                arr     =[]
+                $this.loadList(arr_sch)
+                $this.loadList(arr)
+            }
+        },
+        searchstudent: function(){
+            const $this = this
+
+            $SearchBtn.onclick = ()=>{
+
+            var $name   =   $('#name-sch').value
+            var $gender =   $('#gender-sch').value
+            var $mark   =   $('#mark-sch').value
+            var $sub1   =   $('#sub1-sch').value
+            var $sub2   =   $('#sub2-sch').value
+            
+            var check1 = $mark.match(/[>]/) ? true : false
+            var check2 = $mark.match(/[0-9]/)[0]
+
+            var newArr = mn.list.filter((e)=>{
+                if($mark == 10){
+                  return e.gender == $gender && e.mark == 10
+                }
+                else if(check1){
+                  return e.gender == $gender && e.mark >= check2
+                }else{
+                  return e.gender == $gender && e.mark <= check2  
+                }
+            })
+
+            for (var i = 0;i<newArr.length;i++){
+                arr_sch.push(
+                    `
+                <div class="row mt-5">
+                    <div class="col-2 box-1 bd-grey">${newArr[i].name}</div>
+                    <div class="col-2 box-1 bd-grey">${newArr[i].gender}</div>
+                    <div class="col-2 box-1 bd-grey">${newArr[i].mark}</div>
+                    <div class="col-1 box-1 bd-grey">Toan:${newArr[i].subject.subject[0].mark}</div>
+                    <div class="col-1 box-1 bd-grey">Ly:${newArr[i].subject.subject[1].mark}</div>
+                </div>
+                    `
+                )
+            }
+            $this.loadList(arr_sch) 
+            arr_sch = []
+            }  
+        },
+
+        addStudents: function(){
+            const $this = this
+
+            $addBtn.onclick = ()=>{
+
+            var $name = $('#name').value
+            var $gender = $('#gender').value
+            var $mark = Number($('#mark').value)
+            var $sub1 = Number($('#sub1').value)
+            var $sub2 = Number($('#sub2').value)
+            var stu = new Student($name,$gender,$mark,new Subject(sub1,$sub1,sub2,$sub2))
+            mn.addStudent(stu)
+             arr.push(
+            `
+            <div class="row mt-5">
+                <div class="col-2 box-1 bd-grey">${$name}</div>
+                <div class="col-2 box-1 bd-grey">${$gender}</div>
+                <div class="col-2 box-1 bd-grey">${$mark}</div>
+                <div class="col-1 box-1 bd-grey">Toan:${$sub1}</div>
+                <div class="col-1 box-1 bd-grey">Ly:${$sub2}</div>
+            </div>
+
+            `)
+            $this.loadList(arr) 
+            }
+            
+        },
+        sortByASC: function(){
+            const $this = this
+
+            $DESC.onclick = ()=>{
+            mn.sortByASC()
+            arr=[]
+            
+            for(var i = 0;i<mn.list.length;i++){
+                arr.push(
+                `
+            <div class="row mt-5">
+                <div class="col-2 box-1 bd-grey">${mn.list[i].name}</div>
+                <div class="col-2 box-1 bd-grey">${mn.list[i].gender}</div>
+                <div class="col-2 box-1 bd-grey">${mn.list[i].mark}</div>
+                <div class="col-1 box-1 bd-grey">Toan:${mn.list[i].subject.subject[0].mark}</div>
+                <div class="col-1 box-1 bd-grey">Ly:${mn.list[i].subject.subject[1].mark}</div>
+            </div>
+                `)
+            }
+
+         $this.loadList(arr) 
+         
+            }
+        },
+        sortByDESC: function(){
+            const $this = this
+
+            $ASC.onclick = ()=>{
+            mn.sortByDESC()
+            arr=[]
+            
+            for(var i = 0;i<mn.list.length;i++){
+                arr.push(
+                `
+            <div class="row mt-5">
+                <div class="col-2 box-1 bd-grey">${mn.list[i].name}</div>
+                <div class="col-2 box-1 bd-grey">${mn.list[i].gender}</div>
+                <div class="col-2 box-1 bd-grey">${mn.list[i].mark}</div>
+                <div class="col-1 box-1 bd-grey">Toan:${mn.list[i].subject.subject[0].mark}</div>
+                <div class="col-1 box-1 bd-grey">Ly:${mn.list[i].subject.subject[1].mark}</div>
+            </div>
+                `)
+            }
+
+         $this.loadList(arr) 
+            }
+        },
+        loadList: function(array){
+            // const $list = $('.list')
+            $('.list').innerHTML = array.join('')
+        },
+        start: function(){
+            this.addStudents()
+            this.sortByASC()
+            this.sortByDESC()
+            this.searchstudent()
+            this.reset()
+        }
+    }
+    app.start()
+  
